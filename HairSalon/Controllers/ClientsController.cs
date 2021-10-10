3c781d840f6d3b52df02e7ee652f66d385816a99
Shaun Kent <shaunkent81@gmail.com>
2021-10-09 19:59:43 -0700

    add create methods and routes for Clients class

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,5 +21,27 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.Include(client => client.Stylists).ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.Stylists = _db.Stylists.ToList();
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    
   }
 }
